a56956533945368687858bbe4ba4fa19a28a71ee
Jenn2004 <jenifer.j2004@gmail.com>
2026-07-25 18:45:48 +0530

    fix the error in backend folder

--- backend/src/main/java/com/goldengrove/dto/CartItemRequest.java
+++ backend/src/main/java/com/goldengrove/dto/CartItemRequest.java
@@ -1,6 +1,6 @@
 package com.goldengrove.dto;
 
-import jakarta.validation.constraints.Max;
+// import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
